c6398d436ed02264d5c2b89b1eef383f1c4f1117
willconstable <will_constable@msn.com>
2020-04-30 17:04:54 +0100

    tweak made

--- development/app.581aa3f0.js
+++ development/app.581aa3f0.js
@@ -2570,7 +2570,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49646" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50959" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
